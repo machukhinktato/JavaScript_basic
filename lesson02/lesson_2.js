1db839e53d206b8be157ba353ce4b0f67f9e5709
machukhinktato <tarabrinmv@gmail.com>
2020-12-21 18:27:30 +0300

    done

---
+++ lesson02/lesson_2.js
@@ -0,0 +1,11 @@
+function compare_numbers(x, y){
+   if (x > y)
+      console.log("x > y");
+   else if (x < y)
+      console.log("x < y");
+   else
+      console.log("x = y");
+}
+compare_numbers(10, 20);
+compare_numbers(20, 10);
+compare_numbers(20, 20);
