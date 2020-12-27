c12fb18e1ba13d6b93404d99cfe87bb8c9a44bf2
machukhinktato <tarabrinmv@gmail.com>
2020-12-27 20:59:30 +0300

    done

--- lesson05/src/components/index/basket.js
+++ lesson05/src/components/index/basket.js
@@ -1,26 +1,36 @@
-function initBasket(){
+/*jshint esversion: 6 */
+'use strict';
+
+function initBasket() {
     let TITLES = [
         'MANGO PEOPLE T-SHIRT',
         'BANANA PEOPLE T-SHIRT',
     ];
 }
 
-    let PRICES = [52,68];
-    let AMOUNT = [4,2];
+let PRICES = [52, 68];
+let AMOUNT = [4, 2];
 
-    const basket = {
-        items: [],
-        total: null,
-        container: null,
-        wrapper: null,
-        sum: 0,
-        totalContainer: null,
-        init(){
-            this.container = document.querySelector('#basket-items');
-            this.wrapper = document.querySelector('#basket-inner');
-            this.totalContainer = document.querySelector('#basket-sum');
-            this.items =getBasketItems(TITLES, PRICES, AMOUNT);
-            this._render();
-        },
-
-    }
+const basket = {
+    items: [],
+    total: null,
+    container: null,
+    wrapper: null,
+    sum: 0,
+    totalContainer: null,
+    init() {
+        this.container = document.querySelector('#basket-items');
+        this.wrapper = document.querySelector('#basket-inner');
+        this.totalContainer = document.querySelector('#basket-sum');
+        this.items = getBasketItems(TITLES, PRICES, AMOUNT);
+        this._render();
+    },
+    _render() {
+        let htmlStr = '';
+        this.items.forEach((item, i) => {
+            htmlStr += renderBasketTemplate(item, i);
+        });
+        this.container.innerHTML = htmlStr;
+        this._calcSum();
+    },
+}
