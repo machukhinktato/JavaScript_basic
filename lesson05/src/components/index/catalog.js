08fc1dd5603df93e751f5d48a81ec3181c2f0eb0
machukhinktato <tarabrinmv@gmail.com>
2020-12-28 18:50:59 +0300

    done

--- lesson05/src/components/index/catalog.js
+++ lesson05/src/components/index/catalog.js
@@ -1,52 +1,55 @@
-let TITLES = [
-    'MANGO PEOPLE T-SHIRT',
-    'BANANA PEOPLE T-SHIRT',
-    'POTATO PEOPLE T-SHIRT',
-    'CUCUMBER PEOPLE T-SHIRT',
-    'PEPPER PEOPLE T-SHIRT',
-    'GOROKCH PEOPLE T-SHIRT',
-    'ORANGE PEOPLE T-SHIRT',
-    'KIWI PEOPLE T-SHIRT'
-];
-let PRICES = [52, 68, 36.1, 700, 87, 50, 67.5, 120.03];
+/*jshint esversion: 6 */
+'use strict';
 
-const catalog = {
-    items: [],
-    container: null,
-    init() {
-        this.container = document.querySelector('#catalog');
-        this.items = getItems();
-        this._render();
-    },
-    _render() {
-        let htmlStr = '';
 
-        this.items.forEach((item, i) => {
-            htmlStr += `
-                <div class="featuredItem">
-                    <div class="featuredImgWrap">
-                        <div class="featuredBuy">
-                            <button>
-                                <img src="../src/assets/images/addToCart.png" alt="">
-                                Add to Cart
-                            </button>
-                        </div>
-                        <img class="featuredProduct" src="../src/assets/images/featuredItem${1 + i}.jpg" alt="">
-                    </div>
-                    <div class="featuredNameAndPrice">
-                        <div class="featuredItemName">
-                            ${item.productName}
-                        </div>
-                        <div class="featuredItemPrice">$${item.productPrice}</div>
-                    </div>
-                </div>
-            `
-        });
-        this.container.innerHTML = htmlStr;
-    }
+function initCatalog() {
+    let TITLES = [
+        'MANGO PEOPLE T-SHIRT',
+        'BANANA PEOPLE T-SHIRT',
+        'POTATO PEOPLE T-SHIRT',
+        'CUCUMBER PEOPLE T-SHIRT',
+        'PEPPER PEOPLE T-SHIRT',
+        'GOROKCH PEOPLE T-SHIRT',
+        'ORANGE PEOPLE T-SHIRT',
+        'KIWI PEOPLE T-SHIRT'
+    ];
+    let PRICES = [52, 68, 36.1, 700, 87, 50, 67.5, 120.03];
+
+    const catalog = {
+        items: [],
+        container: null,
+        basket: null,
+        init(basket) {
+            this.container = document.querySelector('#catalog');
+            this.items = getCatalogItems(TITLES, PRICES);
+            this.basket = basket;
+            this._render();
+            this._handleEvents();
+        },
+        _render() {
+            let htmlStr = '';
+
+            this.items.forEach((item, i) => {
+                htmlStr += renderCatalogTemplate(item, i);
+            });
+            this.container.innerHTML = htmlStr;
+        },
+
+        _handleEvents() {
+            this.container.addEventListener('click', event => {
+                if (event.target.name == 'add') {
+                    // console.log('куплено')
+                    let id = event.target.dataset.id;
+                    let item = this.items.find(el => el.productId == id);
+                    item = Object.assign({}, item, {productAmount:1});
+                    this.basket.add(item);
+                }
+            });
+
+        }
+    };
+    return catalog;
 }
-
-catalog.init();
 
 
 function getItems() {
