652c5c12d317de90b2a83a5d3ed3720b423f26d6
machukhinktato <tarabrinmv@gmail.com>
2020-12-30 18:18:29 +0300

    in progress

--- lesson06/onlesson.js
+++ lesson06/onlesson.js
@@ -1,3 +1,9 @@
     function myProccesor() {
         alert('loading complete, fuck yeah it works');
     }
+
+    function changeText(eventObj) {
+        let appDiv = document.getElementById('mega');
+        appDiv.innerHTML = 'SPARTAK ULTRAS'
+    }
+
