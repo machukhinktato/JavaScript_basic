c9179c49671d9b465e0d9c9c8fd8b201871147ad
machukhinktato <tarabrinmv@gmail.com>
2020-12-31 15:24:27 +0300

    in progress

--- lesson06/onlesson.js
+++ lesson06/onlesson.js
@@ -3,11 +3,19 @@ function myProccesor() {
 }
 
 
-init() {
-    let appDiv = document.getElementById('mega');
-    appDiv.addEventListener(onclick, changeText);
-},
-function changeText(eventObj) {
+// init()
+// {
+//     let appDiv = document.getElementById('mega');
+//     appDiv.addEventListener(onclick, changeText);
+//
+//     function changeText(eventObj) {
+//         let appDiv = document.getElementById('mega');
+//         appDiv.innerHTML = 'SPARTAK ULTRAS'
+        appDiv.addEventListener('SPARTAK ULTRAS')
+    // }
+// }
+
+function changeText() {
     let appDiv = document.getElementById('mega');
     appDiv.innerHTML = 'SPARTAK ULTRAS'
     // appDiv.addEventListener('SPARTAK ULTRAS')
