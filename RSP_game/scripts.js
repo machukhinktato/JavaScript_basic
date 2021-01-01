45638328cb39f5954f35d3530f30a4612aad0c87
machukhinktato <tarabrinmv@gmail.com>
2021-01-01 11:17:19 +0300

    in progress

--- RSP_game/scripts.js
+++ RSP_game/scripts.js
@@ -1,9 +1,12 @@
 function init() {
     arr = ['rock', 'scissors', 'paper']
-    
+    result = arr[cpuChoise()]
+    console.log(result)
 }
 
 function cpuChoise() {
     let choise = Math.floor(Math.random() * 3)
     console.log(choise)
+    return choise
 }
+
