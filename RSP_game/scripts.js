d0f328f20f69741abc9fb674faba81c3965a63c6
machukhinktato <tarabrinmv@gmail.com>
2021-01-01 11:50:04 +0300

    in progress

--- RSP_game/scripts.js
+++ RSP_game/scripts.js
@@ -3,7 +3,10 @@ function init() {
     result = arr[cpuChoise()]
     console.log(result)
 }
-
+function userPick() {
+    let choise = [...prompt('enter a number')]
+    console.log(choise)
+}
 function cpuChoise() {
     let choise = Math.floor(Math.random() * 3)
     console.log(choise)
