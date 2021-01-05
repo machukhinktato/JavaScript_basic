ce9fb368e97be6d14a9e34ec41d00a0d82396e37
machukhinktato <tarabrinmv@gmail.com>
2021-01-05 17:40:23 +0300

    in progress

--- RSP_game/scripts.js
+++ RSP_game/scripts.js
@@ -14,7 +14,6 @@ function init(choice) {
 }
 
 function resCheck(usr, cpu, score) {
-    winner = null
     if (usr == cpu) {
         console.log('due')
     } else if ((usr == 'rock' && cpu == 'scissors') || (usr == 'paper' && cpu == 'rock') || (usr == 'scissors' && cpu == 'paper')) {
@@ -27,43 +26,4 @@ function resCheck(usr, cpu, score) {
 function cpuChoise() {
     let choise = Math.floor(Math.random() * 3)
     return choise
-}
-
-
-function rock_game(user) {
-    let terminator_ans = Math.floor(Math.random() * 3);
-    let names = ['Rock', 'Scissors', 'Paper'];
-    let game = {
-        user: user,
-        terminator: terminator_ans,
-        winner: null,
-        check() {
-            let winner = null;
-
-
-            if ((this.user == 0 && this.terminator == 1) || (this.user == 1 && this.terminator == 2) || (this.user == 2 && this.terminator == 0)) {
-                this.winner = 'user';
-            } else if (this.user == this.terminator) {
-                this.winner = 'Both';
-            } else {
-                this.winner = 'pc';
-            }
-
-            this._render();
-        },
-        _render() {
-            let userText = names[this.user];
-            let pcText = names[this.terminator];
-
-            let userSpan = document.querySelector('#user-variant');
-            let pcSpan = document.querySelector('#pc-variant');
-            let resultBlock = document.querySelector('#score');
-
-            userSpan.innerText = userText;
-            pcSpan.innerText = pcText;
-
-            resultBlock.innerHTML += `${this.winner} победил; Человечество: ${userText}, Машинный разум улья: ${pcText}<br>`
-        }
-    };
-    game.check()
 }
