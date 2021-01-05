fd9bc4b2cab029539007bb954c39a1ed01e68212
machukhinktato <tarabrinmv@gmail.com>
2021-01-05 15:02:47 +0300

    in progress

--- RSP_game/scripts.js
+++ RSP_game/scripts.js
@@ -3,8 +3,8 @@ function init(choice) {
         'user': 0,
         'cpu': 0,
     }
-    arr = ['rock', 'scissors', 'paper']
-    while (score.user < 10 && score.cpu < 10) {
+    let arr = ['rock', 'scissors', 'paper']
+    for (let round = 0; round < 10; round++) {
         let userPick = arr[choice]
         let cpuTurn = arr[cpuChoise()]
         let winner = resCheck(userPick, cpuTurn, score)
@@ -14,9 +14,10 @@ function init(choice) {
 }
 
 function resCheck(usr, cpu, score) {
+    winner = null
     if (usr == cpu) {
         console.log('due')
-    } else if (usr == 'rock' && cpu == 'scissors' || usr == 'paper' && cpu == 'rock' || usr == 'scissors' && cpu == 'paper') {
+    } else if ((usr == 'rock' && cpu == 'scissors') || (usr == 'paper' && cpu == 'rock') || (usr == 'scissors' && cpu == 'paper')) {
         score.user += 1
     } else {
         score.cpu += 1
@@ -25,6 +26,44 @@ function resCheck(usr, cpu, score) {
 
 function cpuChoise() {
     let choise = Math.floor(Math.random() * 3)
-    console.log(choise)
     return choise
+}
+
+
+function rock_game(user) {
+    let terminator_ans = Math.floor(Math.random() * 3);
+    let names = ['Rock', 'Scissors', 'Paper'];
+    let game = {
+        user: user,
+        terminator: terminator_ans,
+        winner: null,
+        check() {
+            let winner = null;
+
+
+            if ((this.user == 0 && this.terminator == 1) || (this.user == 1 && this.terminator == 2) || (this.user == 2 && this.terminator == 0)) {
+                this.winner = 'user';
+            } else if (this.user == this.terminator) {
+                this.winner = 'Both';
+            } else {
+                this.winner = 'pc';
+            }
+
+            this._render();
+        },
+        _render() {
+            let userText = names[this.user];
+            let pcText = names[this.terminator];
+
+            let userSpan = document.querySelector('#user-variant');
+            let pcSpan = document.querySelector('#pc-variant');
+            let resultBlock = document.querySelector('#score');
+
+            userSpan.innerText = userText;
+            pcSpan.innerText = pcText;
+
+            resultBlock.innerHTML += `${this.winner} победил; Человечество: ${userText}, Машинный разум улья: ${pcText}<br>`
+        }
+    };
+    game.check()
 }
