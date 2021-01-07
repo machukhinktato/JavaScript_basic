53fd2fcf209cfaae8058f3e60413f50e332108ab
machukhinktato <tarabrinmv@gmail.com>
2021-01-07 16:02:38 +0300

    in progress

--- paint/js/app.js
+++ paint/js/app.js
@@ -12,17 +12,37 @@ const abobe = {
     },
     init() {
         this.context = this.canvas.getContext('2d');
+        this._handleEvents();
+    },
+    _handleEvents() {
         this.canvas.addEventListener('mousemove', this._moveHandler.bind(this));
+        this.canvas.addEventListener('mousedown', this._startProcess.bind(this));
+        document.addEventListener('ouseup', this._endProcess.bind(this));
+        this.tools.addEventListener('click', this._clickHandler.bind(this));
+        this.tools.addEventListener('change', this._changeHandler.bind(this));
     },
     _moveHandler(e) {
         this.x = e.offsetX;
         this.y = e.offsetY;
         this._renderCoordinates();
-        this.context.fillRect(this.x, this.y, this.editor.currentBrushSize, this.editor.currentBrushSize);
+        // this.context.fillRect(this.x, this.y, this.editor.currentBrushSize, this.editor.currentBrushSize);
+    },
+    _clickHandler(e) {
+        if (e.target.tool === 'tool') {
+            this.editor.currentTool = e.target.dataset.tool;
+        }
     },
     _renderCoordinates() {
         document.querySelector('#xCoord').innerText = this.x;
         document.querySelector('#yCoord').innerText = this.y;
+    },
+    _startProcess() {
+        this.canvas.onmousemove = () => {
+            this.context.fillRect(this.x, this.y, this.editor.currentBrushSize, this.editor.currentBrushSize);
+        }
+    },
+    _endProcess() {
+        this.canvas.onmousemove = null;
     }
 }
 
